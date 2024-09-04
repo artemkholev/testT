cc2267895a3764f2e07b6a441467d5fd2b8af4ff
artem-kholev <artemkholev@yandex.ru>
2024-09-04 20:42:36 +0300

    ADD: links header and style

--- src/app/providers/router/index.js
+++ src/app/providers/router/index.js
@@ -2,6 +2,8 @@ import { routes } from './routes'
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
+  linkActiveClass: 'border-bottom',
+  linkExactActiveClass: 'border-bottom_exact',
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
